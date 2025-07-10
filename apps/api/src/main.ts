997ef71b2eeaae007022788c516d6c5fea6a8119
Ademola Aderemi <158827677+cridiv@users.noreply.github.com>
2025-07-10 07:51:17 +0100

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://rave-ai.vercel.app',
     credentials: true,
   });
 
